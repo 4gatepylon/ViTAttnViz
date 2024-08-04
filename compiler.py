94a9152e011d037a99295f1a7a6be5820890003a
Adriano <adrianoh@mit.edu>
2024-08-03 17:57:20 -0700

    Demo looks good, but we need the actual tensors and a way to insert them

---
+++ compiler.py
@@ -0,0 +1,6 @@
+# NOTE these two are hard-coded because they are in the template.
+START_REPLACEMENT_UUID = "4044e411-a988-4834-b453-193d4bbef321"
+END_REPLACEMENT_UUID = "b180600e-4295-4f5d-8b13-bd0992b955f3"
+
+# Fine the line with the uuids
+# replace shit between with our desired lines
